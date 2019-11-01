627a6b4e0a74b8405f158dea5ed03fafcd08f7e2
rockerbacon <vas.vitor.santos@gmail.com>
2019-11-01 10:14:54 -0300

    Removed auto dependency

--- .assertions/dependency_manager/install.sh
+++ .assertions/dependency_manager/install.sh
@@ -2,6 +2,5 @@
 apt-package/install.sh g++
 apt-package/install.sh cmake
 apt-package/install.sh make
-git/install.sh https://github.com/rockerbacon/assertions-test.git e6838d6adba46b7254d922567bccc1707e1840b4
 systemlib/install.sh pthread
 git-source/install.sh https://github.com/rockerbacon/stopwatch.git v1-no-tests
