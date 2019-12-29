b046ee2d2c31a638343b7b430e054f7f539811c9
rockerbacon <vas.vitor.santos@gmail.com>
2019-12-29 19:58:31 -0300

    Marked stopwatch as local only

--- .assertions/dependency_manager/install.sh
+++ .assertions/dependency_manager/install.sh
@@ -2,4 +2,4 @@
 apt-package/install.sh g++
 apt-package/install.sh cmake
 apt-package/install.sh make
-git/install.sh https://github.com/rockerbacon/stopwatch.git v2.1.2 false "src/objs" "src/objs"
+git/install.sh https://github.com/rockerbacon/stopwatch.git v2.1.2 true "src/objs" "src/objs" ""
